d188cf95eff401a40f059ce2ce05a2241ec10240
Scott <scotter355@gmail.com>
2024-07-28 14:02:39 -0400

    Drops from blocks now working

--- src/main/kotlin/example/examplemod/block/ModBlocks.kt
+++ src/main/kotlin/example/examplemod/block/ModBlocks.kt
@@ -7,6 +7,7 @@ import net.minecraft.world.item.Item
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.DropExperienceBlock
+import net.minecraft.world.level.block.SoundType
 import net.minecraft.world.level.block.state.BlockBehaviour
 import net.neoforged.neoforge.registries.DeferredBlock
 import net.neoforged.neoforge.registries.DeferredItem
@@ -14,21 +15,21 @@ import net.neoforged.neoforge.registries.DeferredRegister
 
 /**
  * Register blocks (placeable in the world).
- * Take note how the block is also registered as an item
+ * Take note how the block is also registered as an item to be picked up (loot table drops)
  * If you get an "overload resolution ambiguity" error, include the arrow at the start of the closure.
  * Even though block_item and ore_item are greyed out and say not used, they are still used when game runs.
  * Block properties can be customized or copied from another block
 */
+
+// TODO no ore spawning
 
 object ModBlocks {
     val REGISTER_BLOCKS: DeferredRegister.Blocks = DeferredRegister.createBlocks(ExampleMod.MOD_ID)
     val REGISTER_BLOCK_ITEMS: DeferredRegister.Items = DeferredRegister.createItems(ExampleMod.MOD_ID)
 
     val EXAMPLE_BLOCK: DeferredBlock<Block> = REGISTER_BLOCKS.register("example_block") { ->
-        Block(BlockBehaviour.Properties.of()
-            .lightLevel { 15 }
-            .strength(3.0f, 6.0f)
-            .requiresCorrectToolForDrops())
+        Block(BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_BLOCK))
+
     }
 
     val EXAMPLE_BLOCK_ITEM: DeferredItem<BlockItem> = REGISTER_BLOCK_ITEMS.register("example_block") { ->
@@ -38,10 +39,15 @@ object ModBlocks {
     val EXAMPLE_ORE: DeferredBlock<Block> = REGISTER_BLOCKS.register("example_ore") { ->
         DropExperienceBlock(
             UniformInt.of(8, 15),
-            BlockBehaviour.Properties.ofFullCopy(Blocks.COAL_ORE))
+            BlockBehaviour.Properties.of()
+                .requiresCorrectToolForDrops()
+                .strength(6f)
+                .sound(SoundType.SNOW))
     }
 
     val EXAMPLE_ORE_ITEM: DeferredItem<BlockItem> = REGISTER_BLOCK_ITEMS.register("example_ore") { ->
         BlockItem(EXAMPLE_ORE.get(), Item.Properties().stacksTo(64))
-    }
 }
+}
+
+
